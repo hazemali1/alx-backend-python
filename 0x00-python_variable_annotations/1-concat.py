915c53abd9fbecd1ca64f8d6f6746e4a8c77044c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 14:46:58 +0200

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concat function to concat two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    take two strings and concat them together
+    """
+    return str1 + str2
