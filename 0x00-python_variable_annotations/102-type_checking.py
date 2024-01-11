5d5f2c2e401fd13b2dc7a788334aea39a4107bf1
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 18:20:46 +0200

    mypy

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,7 +3,7 @@
 import typing
 
 
-def zoom_array(lst: typing.Tuple[any, ...], factor: int = 2) -> typing.List:
+def zoom_array(lst: typing.Tuple[typing.any, ...], factor: int = 2) -> typing.List:
     """zoom for array"""
     zoomed_in: typing.Tuple = [
         item for item in lst
