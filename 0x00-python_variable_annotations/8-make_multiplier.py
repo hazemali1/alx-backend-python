bd8eb7a8ab8e52c8566e1289ab58d315f5358c88
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 16:53:19 +0200

    callable

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""typing module for callable"""
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    """make a multiplier return function that multiplies"""
+    def fun(multiplier):
+	    return multiplier * multiplier
+    return fun
