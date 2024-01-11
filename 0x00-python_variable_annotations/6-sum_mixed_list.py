4e9f8d61f3bdfd2bccea96a214229ce4f7e64171
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 16:25:39 +0200

    sum mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""typing module for Union here"""
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.Union[int, float]) -> float:
+    """sum_mixed_list function for sum list"""
+    return sum(mxd_lst)
