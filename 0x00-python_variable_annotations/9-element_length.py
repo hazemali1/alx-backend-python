834f2b9824e8b6332eeac3f3ef10404a2aaf65a1
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 17:10:28 +0200

    iterable and sequence

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
     """element length to get lenght of each sequence in the iterable"""
     return [(i, len(i)) for i in lst]
