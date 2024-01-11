5298051ba412c56bf6edcf525ccb137f03e2ca6e
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 17:58:59 +0200

    iterable and sequence

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""module typing"""
+import typing
+
+
+T = typing.TypeVar('T')
+
+def safely_get_value(dct: typing.Mapping, key: typing.Any,
+                     default: typing.Union[T, None]) -> typing.Union[typing.Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
