57642be1243159bd755b1a2da65b030dcf58664b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 14:51:27 +0200

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+"""
+import math pakcage
+floor function to floor float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    take n float and return integer after floor
+    """
+    return math.floor(n)
