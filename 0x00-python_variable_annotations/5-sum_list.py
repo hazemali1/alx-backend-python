1b8e934a3ca70c850106fc8e15705ff4ec13ad6f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 16:13:46 +0200

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """function to add elements of float list"""
+import typing
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """Returns the sum of list"""
     return sum(input_list)
