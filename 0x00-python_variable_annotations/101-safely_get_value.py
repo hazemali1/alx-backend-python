d05a34166d969c8bd87b63d634becb51ac8dc2e6
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-11 18:01:25 +0200

    TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,7 +8,7 @@ T = typing.TypeVar('T')
 
 def safely_get_value(dct: typing.Mapping, key: typing.Any,
                      default: typing.Union[T, None]
-					 ) -> typing.Union[typing.Any, T]:
+                     ) -> typing.Union[typing.Any, T]:
     """safely get value"""
     if key in dct:
         return dct[key]
