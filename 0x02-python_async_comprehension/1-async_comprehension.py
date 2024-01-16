ebc28df823129acd5a67a316ee898ec659d11e71
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-16 17:13:11 +0200

    Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """async function"""
-async_generator = __import__('0-async_generator').async_generator
 import asyncio
 import typing
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
