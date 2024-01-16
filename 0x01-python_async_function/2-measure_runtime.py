89d036ac9fe486389fbc235e8ebdd81a8491d466
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-16 03:04:23 +0200

    total time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,8 @@ import asyncio
 import time
 
 
-def measure_time(n, max_delay):
+def measure_time(n, max_delay) -> float:
+    """measure time"""
     start = time.time()
     lis = asyncio.run(wait_n(n, max_delay))
     end = time.time()
