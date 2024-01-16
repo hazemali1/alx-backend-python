5589b7857b4751e4c54fd89a3430c6e4d1aeee7f
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-16 17:06:22 +0200

    Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,12 @@
 """async function"""
 import asyncio
 async_generator = __import__('0-async_generator').async_generator
+import  typing
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """async comprehension function"""
-    result = []
+    result: typing.List[float] = []
     async for i in async_generator():
         result.append(i)
     return result
