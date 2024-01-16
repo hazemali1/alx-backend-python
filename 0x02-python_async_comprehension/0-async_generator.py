17d3c574a87c85eb72701f61c2bc6109dae0b03c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-16 16:20:44 +0200

    loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,8 +4,14 @@ import asyncio
 import random
 
 
+async def numbers(numbers):
+    """range of numbers"""
+    for i in range(numbers):
+        yield i
+
+
 async def async_generator():
     """loop"""
-    for _ in range(10):
+    async for _ in numbers(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
