362f9e2f2735fc3738ea1d5777589d05caa7885b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2024-01-16 16:24:19 +0200

    loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,16 +2,11 @@
 """async function"""
 import asyncio
 import random
+import typing
 
 
-async def numbers(numbers):
-    """range of numbers"""
-    for i in range(numbers):
-        yield i
-
-
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """loop"""
-    async for _ in numbers(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
